6186f4bacfe45c59c8af9a0beac5b0fe3e0cf3d0
박다정 <ekwjd990913@gmail.com>
2022-10-11 14:16:39 +0900

    setting

---
+++ src/main/java/com/lion/spring_soundtrack/SpringSoundtrackApplication.java
@@ -0,0 +1,13 @@
+package com.lion.spring_soundtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoundtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoundtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lion/spring_soundtrack/SpringSoundtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.spring_soundtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoundtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
