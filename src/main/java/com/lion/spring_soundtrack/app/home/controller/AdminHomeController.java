65a72d5820929c6bf826ebc39a6012338b5650e9
박다정 <ekwjd990913@gmail.com>
2022-10-12 23:59:24 +0900

    관리자만 관리자 홈 화면을 볼 수 있도록 설정, 관리자 전용 layout 생성

--- src/main/java/com/lion/spring_soundtrack/app/home/controller/AdminHomeController.java
+++ src/main/java/com/lion/spring_soundtrack/app/home/controller/AdminHomeController.java
@@ -1,6 +1,7 @@
 package com.lion.spring_soundtrack.app.home.controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminHomeController {
     @GetMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showIndex() {
         return "redirect:/admin/home/main";
     }
 
     @GetMapping("/home/main")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showMain() {
         return "admin/home/main";
     }
