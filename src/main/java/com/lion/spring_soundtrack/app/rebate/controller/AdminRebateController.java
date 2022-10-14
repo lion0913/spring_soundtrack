6acdf25fb99337dcafb3055c1cbb7e9549f1f29b
박다정 <ekwjd990913@gmail.com>
2022-10-14 11:38:21 +0900

    rebateOrderItem 목록 페이지 구현

--- src/main/java/com/lion/spring_soundtrack/app/rebate/controller/AdminRebateController.java
+++ src/main/java/com/lion/spring_soundtrack/app/rebate/controller/AdminRebateController.java
@@ -1,17 +1,20 @@
 package com.lion.spring_soundtrack.app.rebate.controller;
 
 
+import com.lion.spring_soundtrack.app.rebate.entity.RebateOrderItem;
 import com.lion.spring_soundtrack.app.rebate.service.RebateService;
 import com.lion.spring_soundtrack.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/rebate")
@@ -27,7 +30,6 @@ public class AdminRebateController {
 
     @PostMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ResponseBody
     public String makeData(String yearMonth) {
         rebateService.makeDate(yearMonth);
 
@@ -36,10 +38,13 @@ public class AdminRebateController {
 
     @GetMapping("/rebateOrderItemList")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public String showRebateOrderItemList(String yearMonth) {
+    public String showRebateOrderItemList(String yearMonth, Model model) {
         if (yearMonth == null) {
             yearMonth = "2022-10";
         }
+        List<RebateOrderItem> items = rebateService.findRebateOrderItemsByPayDateIn(yearMonth);
+
+        model.addAttribute("items", items);
 
         return "admin/rebate/rebateOrderItemList";
     }
--- src/main/java/com/lion/spring_soundtrack/app/rebate/repository/RebateOrderItemRepository.java
+++ src/main/java/com/lion/spring_soundtrack/app/rebate/repository/RebateOrderItemRepository.java
@@ -3,8 +3,12 @@ package com.lion.spring_soundtrack.app.rebate.repository;
 import com.lion.spring_soundtrack.app.rebate.entity.RebateOrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface RebateOrderItemRepository extends JpaRepository<RebateOrderItem, Long> {
     Optional<RebateOrderItem> findByOrderItemId(long orderItemId);
+
+    List<RebateOrderItem> findAllByPayDateBetweenOrderByIdAsc(LocalDateTime fromDate, LocalDateTime toDate);
 }
