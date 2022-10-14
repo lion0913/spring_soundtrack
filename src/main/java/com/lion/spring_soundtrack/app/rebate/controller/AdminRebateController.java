022fb3a4ec158dc0dcf40ad1f721422e64d76cb8
박다정 <ekwjd990913@gmail.com>
2022-10-14 10:14:48 +0900

    정산 선택 월에 해당하는 날짜 범위구하

--- src/main/java/com/lion/spring_soundtrack/app/rebate/controller/AdminRebateController.java
+++ src/main/java/com/lion/spring_soundtrack/app/rebate/controller/AdminRebateController.java
@@ -1,10 +1,15 @@
 package com.lion.spring_soundtrack.app.rebate.controller;
 
 
+import com.lion.spring_soundtrack.util.Util;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/admin/rebate")
@@ -14,5 +19,19 @@ public class AdminRebateController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public String showMakeData() {
         return "admin/rebate/makeData";
+    }
+
+    @PostMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    public String makeData(String yearMonth) {
+        int monthEndDay = Util.date.getEndDayOf(yearMonth);
+
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Util.date.parse(fromDateStr);
+        LocalDateTime toDate = Util.date.parse(toDateStr);
+
+        return "fromDateStr : %s<br>toDateStr : %s".formatted(fromDateStr, toDateStr);
     }
 }
